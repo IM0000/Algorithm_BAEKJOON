ec8e9a32624f9a4b1e7c10e96f3da1f5f68743f4
IM0000 <imsang0000@gmail.com>
2020-12-14 23:21:43 +0900

    boj

--- 단계별로_풀어보기/B_08_수학1/BOJ_1712.java
+++ 단계별로_풀어보기/B_08_수학1/BOJ_1712.java
@@ -8,17 +8,18 @@ import java.util.StringTokenizer;
 public class BOJ_1712 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int a,b,c;
+		int a, b, c;
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		a=Integer.parseInt(st.nextToken());
-		b=Integer.parseInt(st.nextToken());
-		c=Integer.parseInt(st.nextToken());
+		a = Integer.parseInt(st.nextToken());
+		b = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
 		System.out.println(BOJ_1712.getBEpoint(a, b, c));
 	}
-	public static int getBEpoint(int a,int b,int c) {
+
+	public static int getBEpoint(int a, int b, int c) {
 		int result = -1;
-		if (c>b) {
-			result = a/(c-b) + 1;
+		if (c > b) {
+			result = a / (c - b) + 1;
 		}
 		return result;
 	}
