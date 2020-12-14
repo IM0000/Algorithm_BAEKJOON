d98ebb556c1bef79bcb3155fd54d02c995d01b6c
IM0000 <imsang0000@gmail.com>
2020-12-15 00:41:07 +0900

    boj solution

--- 단계별로_풀어보기/B_08_수학1/BOJ_2292.java
+++ 단계별로_풀어보기/B_08_수학1/BOJ_2292.java
@@ -8,17 +8,26 @@ public class BOJ_2292 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
-		if (n == 1) {
-			System.out.println(1);
-		} else {
-			for (int k = 1; k < 32000; k++) {
-				int left = (int) (3 * (Math.pow(k - 1, 2)) - 3 * (k - 1) + 1);
-				int right = (int) (3 * (Math.pow(k, 2)) - 3 * (k) + 1);
-				if (left < n && n <= right) {
-					System.out.println(k);
-					break;
-				}
-			}
+		
+		System.out.print(solution(n));
+	}
+	
+	private static int solution(int n) {
+		// 1: 1 (1)
+		// 2 ~ 7 : 2 (6개)
+		// 8 ~ 19 : 3 (12개)
+		// 20 ~ 37 : 4 (18개)
+		// 38 ~ 61 : 5 (24개)
+		// ...생략..
+		// a(n) = a(n-1) + 6(n-1) | a(n): 첫 항
+		if(n == 1) return 1;
+		int i=2;
+		int k=1;
+		
+		while(i<=n) {
+			i += 6*k++;
 		}
+		
+		return k;
 	}
 }
