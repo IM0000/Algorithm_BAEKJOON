e789e017d428d7c43a080634c0cd2dba907022cb
IM0000 <imsang0000@gmail.com>
2020-12-15 22:16:05 +0900

    time over

--- 단계별로_풀어보기/B_08_수학1/BOJ_2869.java
+++ 단계별로_풀어보기/B_08_수학1/BOJ_2869.java
@@ -1,9 +1,31 @@
 package B_08_수학1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class BOJ_2869 {
 
-	public static void main(String[] args) {
-		int i;
+	public static void main(String[] args) throws IOException {
+		int a, b, v;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		a = Integer.parseInt(st.nextToken());
+		b = Integer.parseInt(st.nextToken());
+		v = Integer.parseInt(st.nextToken());
+
+		int halfDay = 0, distance = 0;
+		while (distance < v) {
+			if (halfDay % 2 == 0) {
+				distance += a;
+			} else {
+				distance -= b;
+			}
+			halfDay++;
+		}
+		System.out.println((int) Math.ceil(halfDay / 2.0));
+
 	}
 
 }
